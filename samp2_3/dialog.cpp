8c217eec25a0c4ddffcaf69de85d0019fab7342d
aaron <communicate_aaron@outlook.com>
2024-09-08 14:17:14 +0800

    代码化UI设计

---
+++ samp2_3/dialog.cpp
@@ -0,0 +1,115 @@
+#include "dialog.h"
+
+#include <QHBoxLayout>
+#include <QPushButton>
+
+Dialog::Dialog(QWidget* parent)
+    : QDialog(parent) {
+    InitUI();
+    InitSignalSlots();
+    setWindowTitle("手工创建UI");
+}
+
+Dialog::~Dialog() = default;
+
+void
+Dialog::InitUI() {
+    /// 1. 创建三个复选框，并水平布局
+    check_box_under_    = new QCheckBox("Underline");
+    check_box_italic_   = new QCheckBox("Italic");
+    check_box_bold_     = new QCheckBox("bold");
+    auto* h_box_layout1 = new QHBoxLayout();
+    h_box_layout1->addWidget(check_box_under_);
+    h_box_layout1->addWidget(check_box_bold_);
+    h_box_layout1->addWidget(check_box_italic_);
+
+    /// 2. 创建三个单选框，并水平布局
+    radio_button_black_ = new QRadioButton("Black");
+    radio_button_black_->setCheckable(true);
+    radio_button_blue_  = new QRadioButton("Blue");
+    radio_button_red_   = new QRadioButton("Red");
+    auto* h_box_layout2 = new QHBoxLayout();
+    h_box_layout2->addWidget(radio_button_black_);
+    h_box_layout2->addWidget(radio_button_red_);
+    h_box_layout2->addWidget(radio_button_blue_);
+
+    /// 3. 创建三个逻辑按钮，并水平布局
+    push_button_confirm_ = new QPushButton("确定");
+    push_button_cancel_  = new QPushButton("取消");
+    push_button_quit_    = new QPushButton("退出");
+    auto* h_box_layout3  = new QHBoxLayout();
+    h_box_layout3->addWidget(push_button_confirm_);
+    h_box_layout3->addWidget(push_button_cancel_);
+    h_box_layout3->addWidget(push_button_quit_);
+
+    /// 4. 创建文本框，并设置初始化字体
+    plain_text_edit_ = new QPlainTextEdit;
+    plain_text_edit_->setPlainText("Hello World!\n手工创建界面。");
+    QFont font = plain_text_edit_->font(); /// 获取字体
+    font.setPixelSize(20); /// 修改字体大小
+    plain_text_edit_->setFont(font); /// 设置字体
+
+    /// 5. 创建垂直布局，并设置为主布局
+    auto* v_box_layout = new QVBoxLayout(this);
+    v_box_layout->addLayout(h_box_layout1); /// 字体样式
+    v_box_layout->addLayout(h_box_layout2); /// 字体颜色
+    v_box_layout->addWidget(plain_text_edit_); /// 文本框
+    v_box_layout->addLayout(h_box_layout3); /// 逻辑按钮组合
+    setLayout(v_box_layout); /// 设置为窗口的主布局
+}
+
+void
+Dialog::InitSignalSlots() const {
+    /// 字体设置复选框
+    connect(
+        check_box_under_, SIGNAL(clicked(bool)), this,SLOT(do_check_box_under_(bool)));
+    connect(
+        check_box_italic_, SIGNAL(clicked(bool)), this,SLOT(do_check_box_italic_(bool)));
+    connect(
+        check_box_bold_, SIGNAL(clicked(bool)), this,SLOT(do_check_box_bold_(bool)));
+
+    /// 颜色设置单选框
+    connect(radio_button_black_,SIGNAL(clicked()), this,SLOT(do_SetFontColor()));
+    connect(radio_button_red_,SIGNAL(clicked()), this,SLOT(do_SetFontColor()));
+    connect(radio_button_blue_,SIGNAL(clicked()), this,SLOT(do_SetFontColor()));
+
+    /// 逻辑按钮组合
+    connect(push_button_confirm_,SIGNAL(clicked()), this,SLOT(accept()));
+    connect(push_button_cancel_,SIGNAL(clicked()), this,SLOT(reject()));
+    connect(push_button_quit_,SIGNAL(clicked()), this,SLOT(close()));
+}
+
+
+void
+Dialog::do_check_box_under_(const bool checked) const {
+    QFont font = plain_text_edit_->font();
+    font.setUnderline(checked);
+    plain_text_edit_->setFont(font);
+}
+
+void
+Dialog::do_check_box_italic_(const bool checked) const {
+    QFont font = plain_text_edit_->font();
+    font.setItalic(checked);
+    plain_text_edit_->setFont(font);
+}
+
+void
+Dialog::do_check_box_bold_(const bool checked) const {
+    QFont font = plain_text_edit_->font();
+    font.setBold(checked);
+    plain_text_edit_->setFont(font);
+}
+
+void
+Dialog::do_SetFontColor() const {
+    QPalette palette = plain_text_edit_->palette();
+    if (radio_button_black_->isChecked())
+        palette.setColor(QPalette::Text, Qt::black);
+    else if (radio_button_red_->isChecked())
+        palette.setColor(QPalette::Text, Qt::red);
+    else if (radio_button_blue_->isChecked())
+        palette.setColor(QPalette::Text, Qt::blue);
+
+    plain_text_edit_->setPalette(palette);
+}
---
+++ samp2_3/dialog.h
@@ -0,0 +1,55 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QCheckBox>
+#include <QDialog>
+#include <QPlainTextEdit>
+#include <QRadioButton>
+
+QT_BEGIN_NAMESPACE
+
+namespace Ui
+{
+class Dialog;
+}
+
+QT_END_NAMESPACE
+
+class Dialog : public QDialog {
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget* parent = nullptr);
+
+    ~Dialog() override;
+
+private:
+    Ui::Dialog* ui{};
+    /// 文本格式复选框
+    QCheckBox* check_box_under_{};
+    QCheckBox* check_box_italic_{};
+    QCheckBox* check_box_bold_{};
+    /// 颜色选择按钮
+    QRadioButton* radio_button_black_{};
+    QRadioButton* radio_button_red_{};
+    QRadioButton* radio_button_blue_{};
+
+    /// 逻辑按钮
+    QPushButton* push_button_confirm_{}; /// 确定
+    QPushButton* push_button_cancel_{}; /// 取消
+    QPushButton* push_button_quit_{}; /// 退出
+
+    /// 文本框
+    QPlainTextEdit* plain_text_edit_{};
+
+    void InitUI(); /// 初始化UI
+    void InitSignalSlots() const; /// 初始化信号与槽的连接
+
+private slots:
+    void do_check_box_under_(bool checked) const; /// 下划线复选框
+    void do_check_box_italic_(bool checked) const; /// 斜体复选框
+    void do_check_box_bold_(bool checked) const; /// 加粗复选框
+    void do_SetFontColor() const; /// 设置文字颜色
+
+};
+#endif  // DIALOG_H
