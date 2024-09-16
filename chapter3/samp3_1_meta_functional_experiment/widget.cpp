b23fac9d3b290ddf307cb69fb5f44f2dd0d5d520
aaron <communicate_aaron@outlook.com>
2024-09-17 00:55:44 +0800

    修改clang-format并添加Qt foreach宏

--- chapter3/samp3_1_meta_functional_experiment/widget.cpp
+++ chapter3/samp3_1_meta_functional_experiment/widget.cpp
@@ -20,7 +20,7 @@ Widget::Widget(QWidget* parent)
     boy->setProperty("age", 20);
     boy->setProperty("score", 95);
     boy->setProperty("sex", "Boy"); /// 动态属性
-                                               /// 动态属性针对类的实例定义
+                                                 /// 动态属性针对类的实例定义
     connect(boy, &TPerson::ageChanged, this, &Widget::do_ageChanged);
 
     /// 女孩
@@ -34,8 +34,12 @@ Widget::Widget(QWidget* parent)
     ui->spinbox_boy->setProperty("isBoy", true); /// 动态属性isBoy
     ui->spinbox_girl->setProperty("isGirl", false);
     /// 连接（以两种方式表示）
-    connect(ui->spinbox_boy, &QSpinBox::valueChanged, this, &Widget::do_spinChanged);
-    connect(ui->spinbox_girl, SIGNAL(valueChanged(int)), this, SLOT(do_spinChanged(int)));
+    connect(
+        ui->spinbox_boy, &QSpinBox::valueChanged, this,
+        &Widget::do_spinChanged);
+    connect(
+        ui->spinbox_girl, SIGNAL(valueChanged(int)), this,
+        SLOT(do_spinChanged(int)));
 }
 
 Widget::~Widget() { delete ui; }
@@ -48,14 +52,16 @@ Widget::do_ageChanged(const int age) const {
     const auto person_sex  = person->property("sex").toString();
     const int  person_age  = person->age();
     /// person_age             = person->property("age").toInt(); /// 通过属性获得
-    const auto person_info = QString("name: %1,\t sex: %2,\t age: %3\n").arg(person_name).arg(person_sex).arg(
-        person_age);
+    const auto person_info = QString("name: %1,\t sex: %2,\t age: %3\n").
+        arg(person_name).arg(person_sex).arg(
+            person_age);
     ui->plaintext_edit->appendPlainText(person_info);
 }
 
 void
 Widget::do_spinChanged(const int age) const {
-    if (const auto spin_box = qobject_cast<QSpinBox*>(sender()); spin_box->property("isBoy").toBool()) {
+    if (const auto spin_box = qobject_cast<QSpinBox*>(sender()); spin_box->
+        property("isBoy").toBool()) {
         /// 根据isBoy判断，避免代码冗余
         boy->setAge(age);
     } else { girl->setAge(age); }
@@ -75,7 +81,8 @@ Widget::on_pushbutton_display_metainfo_clicked() const {
     const auto obj  = boy;
     const auto meta = obj->metaObject();
     ui->plaintext_edit->clear();
-    ui->plaintext_edit->appendPlainText(QString("-----------\nClass name: %1\n").arg(meta->className()));
+    ui->plaintext_edit->appendPlainText(
+        QString("-----------\nClass name: %1\n").arg(meta->className()));
     /// 打印属性信息
     ui->plaintext_edit->appendPlainText("-----------\nProperties:\n");
     print_properties(boy);
@@ -87,7 +94,9 @@ Widget::on_pushbutton_display_metainfo_clicked() const {
     for (int i = meta->classInfoOffset(); i < meta->classInfoCount(); ++i) {
         auto class_info = meta->classInfo(i);
         ui->plaintext_edit->appendPlainText(
-            QString("name: %1,\t value: %2\n").arg(class_info.name(), class_info.value()));
+            QString("name: %1,\t value: %2\n").arg(
+                class_info.name(),
+                class_info.value()));
     }
     ui->plaintext_edit->appendPlainText("-----------\n");
 
@@ -100,11 +109,14 @@ void
 Widget::print_properties(const QObject* obj) const {
     const auto meta = obj->metaObject();
     for (int i = meta->propertyOffset(); i < meta->propertyCount(); ++i) {
-        const auto property_name   = meta->property(i).name(); /// 需含<QtCore/QMetaProperty>头文件
+        const auto property_name = meta->property(i).name();
+        /// 需含<QtCore/QMetaProperty>头文件
         const auto property_value  = obj->property(property_name).toString();
-        const auto properties_info = QString("property type: %1,\t value: %2\n").arg(
-            property_name,
-            property_value);
+        const auto properties_info = QString("property type: %1,\t value: %2\n")
+            .
+            arg(
+                property_name,
+                property_value);
         ui->plaintext_edit->appendPlainText(properties_info);
     }
 }
@@ -130,7 +142,8 @@ Widget::TestIterator() {
      */
     QList<QString> list_string;
     list_string << "A" << "B" << "C" << "D" << "E" << "F";
-    for (QList<QString>::const_iterator iterator = list_string.constBegin(); iterator != list_string.constEnd(); ++
+    for (QList<QString>::const_iterator iterator = list_string.constBegin();
+         iterator != list_string.constEnd(); ++
          iterator) {
         /// 只读迭代器，逐项读出
         qDebug() << *iterator; /// Debug优先级更高，会在缓冲区中优先读出。
@@ -149,9 +162,11 @@ Widget::TestIterator() {
     num1.insert("rout", 100000);
     num1.insert("rout", 200000); /// num1.size()=2
     num2.insert("rout", 300000); /// num2.size()=1
-    const QMultiMap<QString, int> num3 = num1 + num2; /// num3.size()=3
+    QMultiMap<QString, int> num3 = num1 + num2; /// num3.size()=3
+    num3.insert("test", 1);
 
-    for (QMultiMap<QString, int>::const_iterator iterator = num3.constBegin(); iterator != num3.constEnd(); ++
+    for (QMultiMap<QString, int>::const_iterator iterator = num3.constBegin();
+         iterator != num3.constEnd(); ++
          iterator) {
         /// 所有项的键和值输出
         qDebug() << iterator.key() << ":" << iterator.value();
@@ -159,6 +174,7 @@ Widget::TestIterator() {
 
     /**
      * 3. foreach 遍历
+     * @remark C++11引入基于范围的循环，Qt不建议继续使用foreach。
      */
     foreach(const QString& value, list_string) {
         /// value必须是后者容器内类型
@@ -170,5 +186,16 @@ Widget::TestIterator() {
     string_hash.insert("Hello", "World");
     string_hash.insert("Welcome", "China");
 
+    foreach(const QString& str, string_hash.keys()) {
+        /// 无需调用values函数
+        qDebug() << str << ":" << string_hash.value(str);
+    }
 
+    foreach(const auto& str, num3.uniqueKeys()) {
+        // 获取唯一键
+        foreach(const auto& num, num3.values(str)) {
+            /// 双重遍历
+            qDebug() << str << ":" << num;
+        }
+    }
 }
