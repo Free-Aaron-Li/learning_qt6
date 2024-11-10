5e0e0de1c8bbfeea01002761d71937a495c10e96
aaron <communicate_aaron@outlook.com>
2024-11-10 14:05:19 +0800

    style: 更新代码格式设置并调整相关代码
    - 修改 .clang-format 文件中的代码对齐和缩进设置
    - 格式化 chapter4/samp4_5_conversion_qstring_number 项目中的代码
    - 优化代码结构，提高可读性和一致性

--- chapter4/samp4_5_conversion_qstring_number/src/test.cpp
+++ chapter4/samp4_5_conversion_qstring_number/src/test.cpp
@@ -9,33 +9,30 @@
 
 #include "test.hpp"
 
-void
-TestItems() {
-    //TestAsprintf();
-    //TestArg();
+void TestItems() {
+    // TestAsprintf();
+    // TestArg();
     TestSetNum();
 }
 
-void
-TestAsprintf() {
+void TestAsprintf() {
     // 格式化字符串，语法类似C语言
-    const QString str1  = QString::asprintf("Year=%d\tMonth=%2d\tDay=%2d\n", 2024, 10, 17);
+    const QString str1 = QString::asprintf("Year=%d\tMonth=%2d\tDay=%2d\n", 2024, 10, 17);
     const QString China = "中国";
     // 字符串本身允许使用中文，但是使用%s替换文本时，该文本仅限于UTF-8的编码，所以需要一个编码步骤。
     const QString str2 = QString::asprintf("你好，%s\n", China.toLocal8Bit().data());
     // const QString    str2  = QString::asprintf("你好，%s\n", China); // 出现乱码，且不能用QString替换为char*类型
-    constexpr double pi   = M_PI;
-    const QString    str3 = QString::asprintf("%.10f\n", pi);
+    constexpr double pi = M_PI;
+    const QString str3 = QString::asprintf("%.10f\n", pi);
 
     qDebug() << str1 << str2 << str3;
 }
 
-void
-TestArg() {
-    constexpr int   year = 2024, month = 10, day = 7;
-    constexpr int   base = 10; // 默认为10进制
-    constexpr QChar ch('0');   // 用于填充字符，默认为空格
-    const QString   China("中国");
+void TestArg() {
+    constexpr int year = 2024, month = 10, day = 7;
+    constexpr int base = 10; // 默认为10进制
+    constexpr QChar ch('0'); // 用于填充字符，默认为空格
+    const QString China("中国");
     // day值填充到2位，不足前面补0；占位符出现顺序可以打乱
     // 2024年10月07日
     const auto str1 = QString("%1年%3月%2日").arg(year).arg(day, 2, base, ch).arg(month);
@@ -49,11 +46,10 @@ TestArg() {
     qDebug() << str3;
 }
 
-void
-TestSetNum() {
+void TestSetNum() {
     constexpr double num = 1234.56789;
-    QString          str;
-    qDebug("num=%f", num);   // 1234.567890
+    QString str;
+    qDebug("num=%f", num); // 1234.567890
     str.setNum(num, 'f', 5); // 小数点后5位【以自然计数法显示】1234.56789
     qDebug("str=%s", str.toLocal8Bit().data());
     str.setNum(num, 'E', 5); // 基数的小数点后5位【以科学计数法显示】1.23457E+03
--- chapter4/samp4_5_conversion_qstring_number/ui/widget.cpp
+++ chapter4/samp4_5_conversion_qstring_number/ui/widget.cpp
@@ -18,7 +18,7 @@ Widget::~Widget() { delete ui; }
 void
 Widget::on_pushButton_calculate_total_price_clicked() const {
     // text()和setText()函数用于QLineEdit控件的读取和写入
-    bool       ok     = true;
+    bool ok = true;
     const auto amount = ui->lineEdit_amount->text().toInt(&ok); // 当输入浮点数，返回值为0
     if (!ok) {
         // 获取当前活跃窗口作为父窗口，注意：不要使用nullptr，因为消息可以随机显示
@@ -28,9 +28,9 @@ Widget::on_pushButton_calculate_total_price_clicked() const {
         // 当用户输入无效数据时不建议立即重新执行计算，而是提示用户更正输入并重新按下控件
         return;
     } else {
-        const auto  unit_price  = ui->lineEdit_unit_price->text().toFloat();
+        const auto unit_price = ui->lineEdit_unit_price->text().toFloat();
         const float total_price = unit_price * static_cast<float>(amount);
-        QString     total_price_str;
+        QString total_price_str;
         total_price_str = total_price_str.setNum(total_price, 'f', 2); // 保留两位小数
         ui->lineEdit_total_price->setText(total_price_str);
     }
@@ -38,7 +38,7 @@ Widget::on_pushButton_calculate_total_price_clicked() const {
 
 void
 Widget::on_pushButton_dec_conversion_clicked() const {
-    bool       ok        = true;
+    bool ok = true;
     const auto dec_value = ui->lineEdit_dec->text().toLong(&ok, 10);
     if (!ok) {
         QMessageBox::warning(QApplication::activeWindow(), "警告", "请输入正确的十进制数字");
@@ -52,7 +52,7 @@ Widget::on_pushButton_dec_conversion_clicked() const {
 
 void
 Widget::on_pushButton_bin_conversion_clicked() const {
-    bool       ok        = true;
+    bool ok = true;
     const auto bin_value = ui->lineEdit_bin->text().toLong(&ok, 2); // 读取二进制
     if (!ok) {
         QMessageBox::warning(QApplication::activeWindow(), "警告", "请输入正确的二进制数字");
@@ -66,7 +66,7 @@ Widget::on_pushButton_bin_conversion_clicked() const {
 
 void
 Widget::on_pushButton_hex_conversion_clicked() const {
-    bool       ok        = true;
+    bool ok = true;
     const auto hex_value = ui->lineEdit_hex->text().toLong(&ok, 16);
     if (!ok) {
         QMessageBox::warning(QApplication::activeWindow(), "警告", "请输入正确的十六进制数字");
